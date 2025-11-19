58fe56e5e267c299d56698a8191facafc395a906
paolobruni-abcd <paolo.bruni@itisarezzo.it>
2025-11-19 12:32:08 +0100

    primo invio

---
+++ garaauto.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.lang.*;
+
+//solo thread con sistema thread
+public class garaauto{
+	
+	static int classifica[] = new int [20];
+
+    public static void main(String[] a){
+
+        int numeroDiThread = 5,i;
+
+        // Istanzia un array di thread
+        Thread[] arrayDiThread = new Thread[numeroDiThread];
+
+        // Inizializza ogni elemento dell'array con un nuovo thread
+        for (i = 0; i < numeroDiThread; i++) {
+            arrayDiThread[i] = new ThreadAuto(i);
+        }
+
+        for (Thread thread : arrayDiThread) {
+            thread.start();
+        }
+
+		try{ 
+			for (Thread thread : arrayDiThread) {
+				thread.join();
+			}
+		} catch(InterruptedException e) {}
+
+
+		// stampa arrai tempi
+        System.out.println("Classifica");           
+		
+        for (i = 0; i < numeroDiThread; i++) {
+            System.out.println("Auto "+i+" tempo "+garaauto.classifica[i]);           
+        }
+
+    }
+}
+
+
+public class ThreadAuto extends Thread {
+    Random random = new Random();
+    int numero;
+   
+    public ThreadAuto(int n){
+        numero=n;
+    }
+   
+    public void run(){
+        int i,tempo;
+        for(i=0;i<5;i++) {
+            tempo = random.nextInt(200,1000);
+			
+			if(i==3 || i==7 || i==12) 	{
+				tempo = random.nextInt(1000,3000);
+			}
+
+			garaauto.classifica[numero]+=tempo;
+            try{ Thread.sleep(tempo); } catch(InterruptedException e) {}
+            System.out.println("Auto numero "+numero+" giro: "+i);           
+        }
+    }
+}
+
+
+
